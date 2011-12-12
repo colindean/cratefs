cb4d36679cb9d937eb2976f3977158bbe02888eb
Colin Dean <git@cad.cx>
2011-12-12 00:23:43 -0500

    some more stubbing and basic methods for CrateDirs

--- lib/cratefs/cratedir.rb
+++ lib/cratefs/cratedir.rb
@@ -15,25 +15,42 @@ http://letscrate.com
 
 class CrateDir < Hash
   #wrapper class
-  
-  def initialize name, mode
 
+  attr_accessor :subdirs
+  attr_accessor :name, :mode, :actime, :modtime, :uid, :gid
+  attr_accessor :cratedir
+  
+  def initialize name, mode, is_root=false
+    subdirs = []
+    @root = is_root
+    @uid = @gid = @actime = @modtime = 0
+    @xattr = Hash.new
+    @name = name
+    @mode = mode | (4 << 12) #this apparently must be done manually
+  end
+
+  def wrap crate_object
+    @cratedir = crate_object
+  end
+
+  def root?
+    @root
   end
 
   def listxattr
-
+    @xattr.each {|k,v| list = list + k + "\0"}
   end
 
   def setxattr name, value, flag
-
+    @xattr[name] = value #TODO: respect flag
   end
 
   def getxattr name
-
+    @xattr[name]
   end
 
   def removexattr name
-
+    @xattr.delete name
   end
 
   def dir_mode
